0fe40b1aac70379f38ec387d030537af4732148c
Mikkel Juul <mju@dmi.dk>
2020-10-30 13:38:00 +0100

    some features re-added (multiple to-fields)

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ import (
 
 var (
 	port     = flag.Int("port", 8080, "Port to listen on.")
-	smtpAddr = flag.String("smtp_server", "", "Default server:port for smtp communications.")
+	smtpAddr = flag.String("smtp-server", "", "Default server:port for smtp communications.")
 	defFrom  = flag.String("from", "", "Default sender address.")
 	defTo    = flag.String("to", "", "Default recipient address.")
 	defSubj  = flag.String("subject", "", "Default message subject.")
 	defMsg   = flag.String("message", "", "Default mail text.")
-	reqFrom  = flag.String("forced_from", "", "If set, this is the sender, regardless of request parameters.")
+	reqFrom  = flag.String("forced-from", "", "If set, this is the sender, regardless of request parameters.")
 )
 
 func main() {
@@ -50,11 +50,24 @@ const (
 )
 
 type mail struct {
-	from, to, subject, body string
+	from, subject, body string
+	to                  []string
 }
 
 func (m *mail) build(lsep string) string {
-	return fmt.Sprintf("From: %s%sTo: %s%sSubject: %s%s%s%s%s", m.from, lsep, m.to, lsep, m.subject, lsep, lsep, m.body, lsep)
+	var (
+		fromTo  = ""
+		subject = ""
+		body    = ""
+	)
+	fromTo = fmt.Sprintf("From: %s%sTo: %s%s", m.from, lsep, m.to, lsep)
+	if m.subject != "" {
+		subject = fmt.Sprintf("Subject: %s%s", m.subject, lsep)
+	}
+	if m.body != "" {
+		body = fmt.Sprintf("%s%s", lsep, m.body)
+	}
+	return fromTo + subject + body
 }
 
 func (m *mail) String() string {
@@ -78,6 +91,13 @@ func (s *SmtpHandler) reqFieldOrDefault(req *http.Request, field string) string 
 	return s.defaults[field]
 }
 
+func (s *SmtpHandler) reqFieldsOrDefault(req *http.Request, field string) []string {
+	if f := req.Form[field]; len(f) != 0 {
+		return f
+	}
+	return strings.Split(s.defaults[field], ",")
+}
+
 func (s *SmtpHandler) newMailFromRequest(req *http.Request) (*mail, error) {
 	var body string
 	if req.Method == http.MethodPost {
@@ -90,23 +110,33 @@ func (s *SmtpHandler) newMailFromRequest(req *http.Request) (*mail, error) {
 	}
 
 	m := &mail{
-		to:      s.reqFieldOrDefault(req, "to"),
+		to:      s.reqFieldsOrDefault(req, "to"),
 		from:    s.reqFieldOrDefault(req, "from"),
 		subject: s.reqFieldOrDefault(req, "subject"),
 		body:    s.reqFieldOrDefault(req, "msg"),
 	}
 
-	if m.body == "" && body != "" {
+	if body != "" {
 		log.Print("Using mail body from POST.")
 		m.body = body
+		if strings.Contains(body, "Subject: ") {
+			m.subject = ""
+		}
+		if strings.Contains(body, "From: ") {
+			m.from = ""
+		}
+		if strings.Contains(body, "To: ") {
+			m.to = []string{""}
+		}
 	}
 
 	if s.lockedFrom != "" {
 		m.from = s.lockedFrom
+		//From/To in the mail body may be set differently - how does that work?
 	}
 
-	if m.to == "" || m.from == "" || m.subject == "" || m.body == "" {
-		return nil, errors.New("Missing field in mail. Set appropriate query params (to, from, subject) and either set msg as query param or send POST body.")
+	if len(m.to) == 0 || m.from == "" {
+		return nil, errors.New("missing fields in mail. Set appropriate parameters: to, from etc.")
 	}
 
 	return m, nil
@@ -116,14 +146,14 @@ func respondError(wr http.ResponseWriter, err error) {
 	wr.Header().Add("Content-Type", "text/plain")
 	wr.WriteHeader(400)
 	// Careful here about leaking info to an attacker...
-	_, _ = fmt.Fprintf(wr, err.Error())
+	_, _ = wr.Write([]byte(err.Error()))
 }
 
 func respondOk(wr http.ResponseWriter, m *mail) {
 	wr.Header().Add("Content-Type", "text/plain")
 	wr.WriteHeader(200)
 
-	wr.Write([]byte(m.String()))
+	_, _ = wr.Write([]byte(m.String()))
 }
 
 func (s *SmtpHandler) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
@@ -134,7 +164,7 @@ func (s *SmtpHandler) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 		respondError(wr, err)
 	}
 
-	if err := smtp.SendMail(s.smtpAddr, nil, m.from, strings.Split(m.to, ","), m.ForData()); err != nil {
+	if err := smtp.SendMail(s.smtpAddr, nil, m.from, m.to, m.ForData()); err != nil {
 		respondError(wr, err)
 	} else {
 		respondOk(wr, m)
